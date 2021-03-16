cbd909c3dd3e0de6d5fb3d1222bbdb32c782912a
Marcus Miyalys <resolutions@riseup.net>
2021-03-16 22:48:36 +0100

    Add maim wrapper, update README

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 Some older and some newer configs and scripts.  
 Let me know if you have ideas for improvements, corrections or if you think something is missing - thanks! :-)
 
-For WM's I've mostly been switching between tiling wms like i3, awesome, dwm and xmonad, 
-and for bars i've used xmobar and i3bar.  
+I generally strive to write my scripts POSIX compliant, with sh as the shell, 
+so they can run with `dash` as it uses less resources.
+
+I've left most comments in as they may be helpful to some of you, as they are to me.
+
+## WM / Distro history
+
+fluxbuntu -> debian lxde -> arch linux
+
+fluxbox -> openbox -> awesome -> dwm -> xmonad -> i3 -> bspwm
+
+dwm_status(?) -> xmobar -> i3bar -> polybar
+
+most switches are mostly just to try something new.
 Since I've been switching WM's I'm trying to take as much configuration out of the WM as possible (fx. hotkeys), 
 so most still works if I switch again.
-
-I generally strive to write my scripts POSIX compliant, with sh as the shell, so they can run with `dash` as it uses
-less resources.
-
-I've left most comments in as they may be helpful to some of you.
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,37 +1,43 @@
 #! /usr/bin/env sh
 
-# Called the symlink to this mim instead of maim to not overwrite access to the regular maim program
-
-DIR=$HOME/shots
+SCREENSHOT_DIR=$HOME/shots
 # The current isodate with hours, minutes and seconds, to ensure a unique filename
 FILENAME=$(date "+%Y-%m-%d-%H%M%S").png
-PTH=$DIR/$FILENAME
+PTH=$SCREENSHOT_DIR/$FILENAME
 PROG="maim"
 #PROG="scrot"
 #PROG="import -window root" # The -s argument below probably wouldn't work with this
+
+help() {
+  printf '%s\n' 'Screenshot program wrapper, pre-setting path and name' \
+                'Prerequisites: imgur.sh for the automatic imgur uploading option' \
+                '-s    - Select region' \
+                '-s -i - Select region, upload to imgur' \
+                '-f    - Fullscreen screenshot' \
+                "-p    - Pass arguments directly to $PROG"
+}
 
 success_msg() {
   msg "Screenshot saved to $FILENAME"
 }
 
-# TODO: Maybe one version of this should automatically upload it to imgur via imgur.sh? And maybe that should also be bound to a shortcut?
-
-# It's \$ below because we NEED to expand DIR but also to NOT expand $f, as that's a parameter intended for the scrot command to deal with. So with \$ this script removes the \ and then scrot gets a literal $f
-# An alternate idea for I solution I had would be to set $f2 = '$f' and then let it expand $f2 and then hopefully scrot would receive $f
-if [ -z "$1" ]; then # Take screenshot of the full screen
-  $PROG $PTH
-  success_msg
+if [ -z "$1" ]; then
+  help
+elif [ "-f" = "$1" ]; then # Take screenshot of the full screen
+  $PROG "$PTH" && success_msg
 elif [ "-s" = "$1" ];  then # Take screenshot of a region, passing no additional arguments
   if [ -z "$2" ]; then
-    $PROG -s $PTH
-    success_msg
-  else
-    $PROG -s $PTH
-    imgur.sh $PTH 2>>$DIR/imgur_delete_paths.txt # For some reason if I $() this into a variable (in order to print it), it gets stuck, so not doing that
-    msg "Screenshot uploaded and the URL is copied to clipboard."
-    msg "Delete URL is $(tail -n 1 -z $DIR/imgur_delete_paths.txt)" # As per dunst defaults, middle click to open this url
+    $PROG -s "$PTH" && success_msg
+  elif [ "-i" = "$2" ]; then
+    $PROG -s "$PTH"
+    imgur.sh "$PTH" 2>>"$SCREENSHOT_DIR/imgur_delete_paths.txt" # For some reason if I $() this into a variable (in order to print it), it gets stuck, so not doing that
+    msg 'Screenshot uploaded and the URL is copied to clipboard.'
+    msg "Delete URL is "$(tail -n 1 -z $SCREENSHOT_DIR/imgur_delete_paths.txt)"" # As per dunst defaults, middle click to open this url
+  else 
+    help
   fi
-else # If any arguments are passed besides -s, pass them all directly to maim, so this is just used to determine where the file is saved and what name it gets.
-  $PROG $PTH "$@"
-  success_msg
+elif [ "-p" = "$1" ]; then # Pass them all directly to the program, so this is just used to determine the file path
+  $PROG "$PTH" "$@" && success_msg
+else 
+  help
 fi
